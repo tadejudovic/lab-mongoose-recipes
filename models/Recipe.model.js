d6b9126d8abbd4b4cc8fc5c6144ecf33a6deaae3
Tadej <udovictadej@gmail.com>
2021-03-13 08:40:56 +0000

    Completed lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: [String],
+  cuisine: { type: String, required: true },
+  dishtype: { type: String, enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'] },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, minimum: 0 },
+  creator: String,
+  created: { type: Date, default: 'today' }
+
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
